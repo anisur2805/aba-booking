6d5a3f58e8f8e03bd6276b1e8fd1f562d3862667
anisur2805 <anisur2805@gmail.com>
2022-05-07 11:23:46 +0600

    showing students data on list page

--- includes/Admin/views/list-student.php
+++ includes/Admin/views/list-student.php
@@ -7,15 +7,15 @@ use Aba_Booking\Student_Data_Table;
     <a href="<?php echo admin_url('admin.php?page=aba-booking-student&action=new') ?>" class="page-title-action">Add New</a>
 
     <?php
-        $subscriber_table = new Student_Data_Table();
+        $student_data_table = new Student_Data_Table();
     ?>
     <div class="wrap">
         <form id="art-search-form" method="GET">
             <input type="hidden" name="page" value="<?php echo $_REQUEST['page']; ?>" />
             <?php
-                $subscriber_table->prepare_items();
-                $subscriber_table->search_box( 'search', 'search_id' );
-                $subscriber_table->display();
+                $student_data_table->prepare_items();
+                $student_data_table->search_box( 'search', 'search_id' );
+                $student_data_table->display();
             ?>
         </form>
     </div>
