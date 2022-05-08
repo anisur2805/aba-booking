92e28396fd5e488512efaa0a83962c6e9ad01204
anisur2805 <anisur2805@gmail.com>
2022-05-09 04:31:42 +0600

    form err check update

--- includes/Traits/Form_Error.php
+++ includes/Traits/Form_Error.php
@@ -9,14 +9,12 @@ trait Form_Error {
     public $errors = [];
     
     function has_error( $key ) {
-        return isset( $this->errors[ $key ] ) ? true : false;
+        // return isset( $this->errors[ $key ] ) ? true : false;
+        return array_key_exists( $key, $this->errors );
     }
 
     function get_error( $key ) { 
-        if( isset( $this->errors[ $key ] ) ) {
-            return $this->errors[ $key ];
-        }
-
-        return false;
+        return array_key_exists( $key, $this->errors ) ? $this->errors[ $key ] : false;
     }
+    
 }
