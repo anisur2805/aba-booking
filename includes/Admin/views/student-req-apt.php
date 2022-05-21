84b3ec98804bb96ad7064786f61d857eeee19609
anisur2805 <anisur2805@gmail.com>
2022-05-21 14:59:16 +0600

    update insert & update db method

--- includes/Admin/views/student-req-apt.php
+++ includes/Admin/views/student-req-apt.php
@@ -1,5 +1,56 @@
+<?php
+
+ $formStructure = [
+  [
+   "name"          => "",
+   "className"     => "aba_form_group",
+   "concreteClass" => $this->has_error( 'name' ) ? ' form-invalid' : '',
+   "label"         => "name",
+   "labelName"     => "Name*",
+   "type"          => "text",
+   "inputClass"    => "regular-text",
+   "isRequire"     => true,
+  ],
+  [
+   "name"          => "",
+   "className"     => "aba_form_group",
+   "concreteClass" => $this->has_error( 'name' ) ? ' form-invalid' : '',
+   "label"         => "name",
+   "labelName"     => "Name*",
+   "type"          => "text",
+   "inputClass"    => "regular-text",
+   "isRequire"     => true,
+  ],
+
+ ];
+
+ echo "<pre>";
+
+ function concreteClass( $className, $concreteClass ) {
+
+  return $className . " " . $concreteClass;
+
+ }
+
+ foreach ( $formStructure as $form ) {
+
+  echo '<div class="' . concreteClass( $form['className'], $form['concreteClass'] ) . '">
+                  <label for="' . $form['label'] . '">' . $form['labelName'] . '</label>
+                  <div>
+                        <input type="text" class="regular-text" name="name" id="name" require />
+                  </div>
+
+            </div>
+            ';
+
+ }
+
+?>
+
+
+
 <div class="wrap">
-    <h1 class="wp-heading-inline"><?php esc_html_e('Request an Appointment', 'aba-booking'); ?></h1>
+    <h1 class="wp-heading-inline"><?php esc_html_e( 'Request an Appointment', 'aba-booking' );?></h1>
       <?php if ( isset( $_GET['requested'] ) ) {?>
             <div class="notice notice-success">
                   <p><?php esc_html_e( 'Request for appointment sent!', 'aba-booking' );?></p>
@@ -12,7 +63,7 @@
                         <input type="text" class="regular-text" name="name" id="name" require />
                   </div>
                   <?php if ( $this->has_error( 'name' ) ) {?>
-                        <p class="description error"> <?php echo $this->get_error( 'name' ); ?> </p>
+                        <p class="description error"><?php echo $this->get_error( 'name' ); ?> </p>
                   <?php }
                   ?>
             </div>
@@ -22,10 +73,10 @@
                         <input type="text" class="regular-text" id="email" name="email" require />
                   </div>
                   <?php if ( $this->has_error( 'email' ) ) {?>
-                        <p class="description error"> <?php echo $this->get_error( 'email' ); ?> </p>
+                        <p class="description error"><?php echo $this->get_error( 'email' ); ?> </p>
                   <?php }?>
             </div>
-            
+
             <div class="aba_form_group">
                   <label for="student_id">Student ID</label>
                   <div>
@@ -39,23 +90,23 @@
                         <input type="text" class="regular-text" id="department" name="department" require />
                   </div>
                   <?php if ( $this->has_error( 'department' ) ) {?>
-                        <p class="description error"> <?php echo $this->get_error( 'department' ); ?> </p>
+                        <p class="description error"><?php echo $this->get_error( 'department' ); ?> </p>
                   <?php }?>
             </div>
-            
+
             <div class="aba_form_group<?php echo $this->has_error( 'message' ) ? ' form-invalid' : ''; ?>">
                   <label for="message">Message*</label>
                   <div>
                         <textarea class="regular-text" id="message" name="message" require></textarea>
                   </div>
                   <?php if ( $this->has_error( 'message' ) ) {?>
-                        <p class="description error"> <?php echo $this->get_error( 'message' ); ?> </p>
+                        <p class="description error"><?php echo $this->get_error( 'message' ); ?> </p>
                   <?php }?>
             </div>
 
             <?php
-            wp_nonce_field( 'req-appt' );
-            submit_button( 'Request', 'primary', 'request_an_appt' );
+             wp_nonce_field( 'req-appt' );
+             submit_button( 'Request', 'primary', 'request_an_appt' );
             ?>
       </form>
 </div>
--- includes/functions.php
+++ includes/functions.php
@@ -7,26 +7,26 @@
  *
  * @return int|WP_Error
  */
-function aba_booking_insert($args = []) {
+function aba_booking_insert( $args = [] ) {
       global $wpdb;
 
-      if (empty($args['name'])) {
-            return new \WP_Error('no-name', __('You must provide a name', 'aba-booking'));
+      if ( empty( $args['name'] ) ) {
+            return new \WP_Error( 'no-name', __( 'You must provide a name', 'aba-booking' ) );
       }
 
       $defaults = [
             "name"       => '',
-            "email"    => '',
+            "email"      => '',
             "created_by" => get_current_user_id(),
-            "created_at" => current_time('mysql'),
+            "created_at" => current_time( 'mysql' ),
       ];
 
-      $data = wp_parse_args($args, $defaults);
+      $data = wp_parse_args( $args, $defaults );
 
-      if (isset($data['id'])) {
+      if ( isset( $data['id'] ) ) {
 
             $id = $data['id'];
-            unset($data['id']);
+            unset( $data['id'] );
 
             $updated = $wpdb->update(
                   "{$wpdb->prefix}aba_booking_subscriber",
@@ -55,8 +55,8 @@ function aba_booking_insert($args = []) {
                   ]
             );
 
-            if (!$inserted) {
-                  return new \WP_Error('failed-to-insert', __('Failed to insert', 'aba-booking'));
+            if ( !$inserted ) {
+                  return new \WP_Error( 'failed-to-insert', __( 'Failed to insert', 'aba-booking' ) );
             }
 
             return $wpdb->insert_id;
@@ -70,7 +70,7 @@ function aba_booking_insert($args = []) {
  *
  * @return array
  */
-function aba_booking_get_addresses($args = []) {
+function aba_booking_get_addresses( $args = [] ) {
       global $wpdb;
 
       $defaults = [
@@ -80,7 +80,7 @@ function aba_booking_get_addresses($args = []) {
             "order"   => "ASC",
       ];
 
-      $args = wp_parse_args($args, $defaults);
+      $args = wp_parse_args( $args, $defaults );
 
       $items = $wpdb->get_results(
             $wpdb->prepare(
@@ -103,7 +103,7 @@ function aba_booking_get_addresses($args = []) {
 function aba_booking_addresses_count() {
       global $wpdb;
 
-      return (int) $wpdb->get_var("SELECT count(id) FROM {$wpdb->prefix}ac_addresses ");
+      return (int) $wpdb->get_var( "SELECT count(id) FROM {$wpdb->prefix}ac_addresses " );
 }
 
 /**
@@ -113,11 +113,11 @@ function aba_booking_addresses_count() {
  *
  * @return object
  */
-function aba_booking_get_students($id) {
+function aba_booking_get_students( $id ) {
       global $wpdb; // Global WPDB class object
 
-      $student = wp_cache_get('abas-' . $id, 'student');
-      if (false === $student) {
+      $student = wp_cache_get( 'abas-' . $id, 'student' );
+      if ( false === $student ) {
             $student = $wpdb->get_row(
                   $wpdb->prepare(  // use prepare for avoid sql injection
                         "SELECT  * FROM {$wpdb->prefix}aba_booking_student WHERE id = %d",
@@ -125,7 +125,7 @@ function aba_booking_get_students($id) {
                   )
             );
 
-            wp_cache_set('abas-' . $id, $student, 'student');
+            wp_cache_set( 'abas-' . $id, $student, 'student' );
       }
       return $student;
 }
@@ -147,32 +147,31 @@ function aba_booking_delete_student( $id ) {
       );
 }
 
-
 /**
  * Register aba-booking image sizes
  */
 function aba_booking_register_image_size() {
-      add_image_size('popup-creator-landscape', 800, 600, true);
-      add_image_size('popup-creator-square', 500, 500, true);
-      add_image_size('popup-creator-thumbnail', 70);
+      add_image_size( 'popup-creator-landscape', 800, 600, true );
+      add_image_size( 'popup-creator-square', 500, 500, true );
+      add_image_size( 'popup-creator-thumbnail', 70 );
 }
 
 aba_booking_register_image_size();
 
-// Form input field and label 
-function aba_booking_inputLabel($for, $name) {
-      echo '<label for="' . $for . '">' . __($name, 'aba-booking') . '</label>';
+// Form input field and label
+function aba_booking_inputLabel( $for, $name ) {
+      echo '<label for="' . $for . '">' . __( $name, 'aba-booking' ) . '</label>';
 }
 
-function aba_booking_input_field($type = "text", $id = '', $name = '', $placeholder = '', $value = '', $class = "regular-text aba_booking_input") {
+function aba_booking_input_field( $type = "text", $id = '', $name = '', $placeholder = '', $value = '', $class = "regular-text aba_booking_input" ) {
       printf(
-            '<input 
-                  type="%s" 
-                  id="%s" 
-                  name="%s" 
-                  placeholder="%s" 
-                  value="%s" 
-                  class="%s" 
+            '<input
+                  type="%s"
+                  id="%s"
+                  name="%s"
+                  placeholder="%s"
+                  value="%s"
+                  class="%s"
                   require
             />',
 
@@ -185,109 +184,119 @@ function aba_booking_input_field($type = "text", $id = '', $name = '', $placehol
       );
 }
 
-
-// Change Add title to 
-function aba_booking_courses_title($title) {
+// Change Add title to
+function aba_booking_courses_title( $title ) {
       $screen = get_current_screen();
 
-      if ('aba_booking_courses' == $screen->post_type) {
-            $title = __('Add Course Name', 'aba-booking');
+      if ( 'aba_booking_courses' == $screen->post_type ) {
+            $title = __( 'Add Course Name', 'aba-booking' );
       }
 
       return $title;
 }
 
-add_filter('enter_title_here', 'aba_booking_courses_title');
+add_filter( 'enter_title_here', 'aba_booking_courses_title' );
 
-
-// Change Add title to 
-function aba_booking_departments_title($title) {
+// Change Add title to
+function aba_booking_departments_title( $title ) {
       $screen = get_current_screen();
 
-      if ('aba_booking_dept' == $screen->post_type) {
-            $title = __('Add Department Name', 'aba-booking');
+      if ( 'aba_booking_dept' == $screen->post_type ) {
+            $title = __( 'Add Department Name', 'aba-booking' );
       }
 
       return $title;
 }
 
-add_filter('enter_title_here', 'aba_booking_departments_title');
+add_filter( 'enter_title_here', 'aba_booking_departments_title' );
 
-// Change Add title to 
-function aba_booking_semester_title($title) {
+// Change Add title to
+function aba_booking_semester_title( $title ) {
       $screen = get_current_screen();
 
-      if ('aba_booking_semester' == $screen->post_type) {
-            $title = __('Add Semester Name', 'aba-booking');
+      if ( 'aba_booking_semester' == $screen->post_type ) {
+            $title = __( 'Add Semester Name', 'aba-booking' );
       }
 
       return $title;
 }
 
-add_filter('enter_title_here', 'aba_booking_semester_title');
+add_filter( 'enter_title_here', 'aba_booking_semester_title' );
 /**
  * Insert users to db
  *
  * @param array $args
  * @return int|WP_Error
  */
-function aba_student_insert($args = []) {
+function aba_student_insert( $args = [] ) {
       global $wpdb;
 
-      if (empty($args['name'])) {
-            return new \WP_Error('no-name', __('You must provide a name', 'aba-booking'));
+      if ( empty( $args['name'] ) ) {
+            return new \WP_Error( 'no-name', __( 'You must provide a name', 'aba-booking' ) );
       }
 
-      if (empty($args['email'])) {
-            return new \WP_Error('no-email', __('You must provide a email address', 'aba-booking'));
+      if ( empty( $args['email'] ) ) {
+            return new \WP_Error( 'no-email', __( 'You must provide a email address', 'aba-booking' ) );
       }
-      if (empty($args['password'])) {
-            return new \WP_Error('no-password', __('You must provide a password number', 'aba-booking'));
-      }
-
-      if (empty($args['student_id'])) {
-            return new \WP_Error('no-student_id', __('You must provide a student_id number', 'aba-booking'));
+      if ( empty( $args['password'] ) ) {
+            return new \WP_Error( 'no-password', __( 'You must provide a password number', 'aba-booking' ) );
       }
 
-      if (empty($args['department'])) {
-            return new \WP_Error('no-department', __('You must provide a department number', 'aba-booking'));
+      if ( empty( $args['student_id'] ) ) {
+            return new \WP_Error( 'no-student_id', __( 'You must provide a student_id number', 'aba-booking' ) );
+      }
+
+      if ( empty( $args['department'] ) ) {
+            return new \WP_Error( 'no-department', __( 'You must provide a department number', 'aba-booking' ) );
       }
 
       $defaults = [
-            'name'            => '',
-            'email'           => '',
-            'password'        => '',
-            'student_id'      => '',
-            'department'      => '',
-            'created_by'      => get_current_user_id(),
-            'created_at'      => current_time('mysql'),
+            'name'       => '',
+            'email'      => '',
+            'password'   => '',
+            'student_id' => '',
+            'department' => '',
+            'created_by' => get_current_user_id(),
+            'created_at' => current_time( 'mysql' ),
       ];
 
-      $data = wp_parse_args($args, $defaults);
+      $data = wp_parse_args( $args, $defaults );
 
       if ( isset( $data['id'] ) ) {
 
             $id = $data['id'];
             unset( $data['id'] );
-          
+
             $updated = $wpdb->update(
-             "{$wpdb->prefix}aba_booking_student",
-             $data,
-             ['id' => $id],
-             [
-                  '%s',
-                  '%s',
-                  '%s',
-                  '%s',
-                  '%s',
-                  '%d',
-                  '%s',
-             ],
-             ['%d']
+                  "{$wpdb->prefix}aba_booking_student",
+                  $data,
+                  ['id' => $id],
+                  [
+                        '%s',
+                        '%s',
+                        '%s',
+                        '%s',
+                        '%s',
+                        '%d',
+                        '%s',
+                  ],
+                  ['%d']
             );
-          
+
             return $updated;
       } else {
+
+            // globalInsert('aba_booking_student', $data,[
+            //       '%s',
+            //       '%s',
+            //       '%s',
+            //       '%s',
+            //       '%s',
+            //       '%d',
+            //       '%s',
+            // ]);
+
+
             $inserted = $wpdb->insert(
                   "{$wpdb->prefix}aba_booking_student",
                   $data,
@@ -302,29 +311,28 @@ function aba_student_insert($args = []) {
                   ]
             );
 
-            if (!$inserted) {
-                  return new \WP_Error('failed-to-insert', __('Failed to insert', 'aba-booking'));
+            if ( !$inserted ) {
+                  return new \WP_Error( 'failed-to-insert', __( 'Failed to insert', 'aba-booking' ) );
             }
 
             return $wpdb->insert_id;
       }
 }
 
-
 /* Create Staff Member User Role */
 add_role(
       'aba_teacher', //  System name of the role.
-      __('Teacher'), // Display name of the role.
+      __( 'Teacher' ), // Display name of the role.
       array(
-            'read'  => true,
-            'delete_posts'  => true,
+            'read'                   => true,
+            'delete_posts'           => true,
             'delete_published_posts' => true,
-            'edit_posts'   => true,
-            'publish_posts' => true,
-            'upload_files'  => true,
-            'edit_pages'  => true,
-            'edit_published_pages'  =>  true,
-            'publish_pages'  => true,
+            'edit_posts'             => true,
+            'publish_posts'          => true,
+            'upload_files'           => true,
+            'edit_pages'             => true,
+            'edit_published_pages'   => true,
+            'publish_pages'          => true,
             'delete_published_pages' => false, // This user will NOT be able to  delete published pages.
       )
 );
@@ -332,51 +340,87 @@ add_role(
 /* Create Staff Member User Role */
 add_role(
       'aba_student', //  System name of the role.
-      __('Student'), // Display name of the role.
+      __( 'Student' ), // Display name of the role.
       array(
-            'read'  => true,
-            'delete_posts'  => true,
+            'read'                   => true,
+            'delete_posts'           => true,
             'delete_published_posts' => true,
-            'edit_posts'   => true,
-            'publish_posts' => true,
-            'upload_files'  => true,
-            'edit_pages'  => true,
-            'edit_published_pages'  =>  true,
-            'publish_pages'  => true,
+            'edit_posts'             => true,
+            'publish_posts'          => true,
+            'upload_files'           => true,
+            'edit_pages'             => true,
+            'edit_published_pages'   => true,
+            'publish_pages'          => true,
             'delete_published_pages' => false, // This user will NOT be able to  delete published pages.
       )
 );
 
-
 /* Upgrade the Author Role */
 function author_level_up() {
       // Retrieve the  Author role.
-      $role = get_role(  'aba_teacher' );
-      
-      // Let's add a set  of new capabilities we want Authors to have.
-      $role->add_cap(  'edit_pages' );
-      $role->add_cap(  'edit_published_pages' );
-      $role->add_cap(  'publish_pages' );
-  }
-  add_action( 'admin_init', 'author_level_up');
+      $role = get_role( 'aba_teacher' );
 
-  /**
-   * Generate Student id
-   * ex. 201811050101
-   */
+      // Let's add a set  of new capabilities we want Authors to have.
+      $role->add_cap( 'edit_pages' );
+      $role->add_cap( 'edit_published_pages' );
+      $role->add_cap( 'publish_pages' );
+}
+
+add_action( 'admin_init', 'author_level_up' );
+
+/**
+ * Generate Student id
+ * ex. 201811050101
+ */
 function get_student_roll() {
       global $wpdb;
-      $id = 'id';
+      $id         = 'id';
       $student_id = $wpdb->get_row( $wpdb->prepare( "SELECT student_id FROM {$wpdb->prefix}aba_booking_student ORDER BY %s DESC", $id ), ARRAY_A );
-      
+
       $roll_start = 1;
       $date       = date( 'Ymd' );
 
       $validate_student_id = isset( $student_id['student_id'] ) ? $student_id['student_id'] : 1;
-      $init_student_id = $date . $roll_start;
-      $exists_id = $validate_student_id + $roll_start;      
-      $std_id_res = isset( $student_id['student_id'] ) ? $exists_id : $init_student_id;
+      $init_student_id     = $date . $roll_start;
+      $exists_id           = $validate_student_id + $roll_start;
+      $std_id_res          = isset( $student_id['student_id'] ) ? $exists_id : $init_student_id;
 
       return $std_id_res;
 }
+
 get_student_roll();
+
+function globalInsert($tableName, $data = [], $format = null ) {
+      global $wpdb;
+
+      if ( 0 == count( $data ) ) {
+            return false;
+      }
+
+      $inserted = $wpdb->insert("{$wpdb->prefix}". $tableName,$data, $format);
+      
+      if (!$inserted ) {
+            return new \WP_Error( 'failed-to-insert', __( 'Failed to insert', 'aba-booking' ) );
+      }
+
+      return $wpdb->insert_id; 
+}
+
+
+
+
+function globalUpdate($tableName, $update = [], $where, $format = null, $whereFormat = null ) {
+      global $wpdb;
+
+      if ( 0 == count( $update ) ) {
+            return false;
+      } 
+      
+      return $wpdb->update(
+                  "{$wpdb->prefix}". $tableName,
+                  $update,
+                  $where,
+                  $format,
+                  $whereFormat
+            );    
+}
